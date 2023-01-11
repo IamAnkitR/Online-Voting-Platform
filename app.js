8371b350313672e5728472ae37b717bc4e69c4cf
IamAnkitR <ardb8755@gmail.com>
2023-01-11 21:33:57 +0530

    required views and models added

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (request, response) => {
-    response.send("hello world");
-  });
-  
-  module.exports = app;
+app.get("/", (req, res) => {
+    return res.render("index");
+});
+
+app.get("/signup", (req, res) => {
+  return res.render("signup");
+})
+
+app.get("/signin", (req, res) => {
+  return res.render("signin");
+})
+
+module.exports = app;
---
+++ migrations/20230111075541-create-election.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Elections', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      started: {
+        type: Sequelize.BOOLEAN
+      },
+      ended: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Elections');
+  }
+};
