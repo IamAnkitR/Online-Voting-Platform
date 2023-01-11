671b5242055d827736ddb1a07e91ba40dfe8a61f
IamAnkitR <ardb8755@gmail.com>
2023-01-12 01:24:48 +0530

    migration fix

--- migrations/20230111075655-adminID-in-election.js
+++ migrations/20230111075655-adminID-in-election.js
@@ -30,6 +30,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn("Elections", "adminID");
+    await queryInterface.removeColumn("Election", "adminID");
   }
 };
